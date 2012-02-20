788739973bf3804f8b287283f4617ea1bb012237
nils <birkeland@gmx.net>
2012-02-21 00:39:23 +0100

    New implementation of the Fhotibot. Subsumption architecture with drive and finder layers. Finder with remote control integration.

---
+++ Robots/Fhotibot2/Fhotibot2.ino
@@ -0,0 +1,265 @@
+/* Raw IR commander
+ 
+ This sketch/program uses the Arduno and a PNA4602 to 
+ decode IR received.  It then attempts to match it to a previously
+ recorded IR signal
+ 
+ Code is public domain, check out www.ladyada.net and adafruit.com
+ for more tutorials! 
+ */
+
+// We need to use the 'raw' pin reading methods
+// because timing is very important here and the digitalRead()
+// procedure is slower!
+//uint8_t IRpin = 2;
+// Digital pin #2 is the same as Pin D2 see
+// http://arduino.cc/en/Hacking/PinMapping168 for the 'raw' pin mapping
+#define IRpin_PIN      PIND
+#define IRpin          2
+
+// the maximum pulse 65000 we'll listen for - 65 milliseconds is a long time
+#define MAXPULSE 4178
+
+// what our timing resolution should be, larger is better
+// as its more 'precise' - but too large and you wont get
+// accurate timing
+#define RESOLUTION 20 
+
+// What percent we will allow in variation to match the same code
+#define FUZZINESS 20
+
+// we will store up to 100 pulse pairs (this is -a lot-)
+uint16_t pulses[100][2];  // pair is high and low pulse 
+uint8_t currentpulse = 0; // index for pulses we're storing
+
+extern "C"{
+  #include "sTheUnit.h"
+}
+#include "ircodesSilverlitPicooZ2006.h"
+#include <AFMotor.h>
+
+AF_DCMotor motorLeft(1);
+AF_DCMotor motorRight(2);
+
+int theSpeed = 200;
+unsigned char lastEvent = C1_;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("Ready to decode IR!");
+  motorLeft.setSpeed(theSpeed);     // set the speed to 200/255
+  motorRight.setSpeed(theSpeed);     // set the speed to 200/255
+
+  if (fINIT_())
+  {
+  	/* activation of drive loop */
+  }
+  else 
+  {
+  	/* initialization failed, abort drive */
+  }
+  
+}
+
+void loop(void) {
+  int numberpulses;
+  
+  numberpulses = listenForIR();
+  
+  Serial.print("Heard ");
+  Serial.print(numberpulses);
+  Serial.println("-pulse long IR signal");
+  if(numberpulses<21){return;}
+
+  if (IRcompare(numberpulses, IRsignal_LEFT_ChannelA)) {
+    Serial.println("LEFT");
+    notify(C1_Left);
+  }
+
+  if (IRcompare(numberpulses, IRsignal_RIGHT_ChannelA)) {
+    Serial.println("RIGHT");
+    notify(C1_Right);
+  }
+
+  if (IRcompare(numberpulses, IRsignal_FORWARD_FULL_ChannelA)) {
+    Serial.println("AHEAD");
+    notify(C1_Ahead);
+  }
+
+  if (IRcompare(numberpulses, IRsignal_STOP_ChannelA)) {
+    Serial.println("STOP");
+    notify(C1_Unknown);
+  }
+}
+
+void notify(unsigned char event){
+  if(lastEvent == event){
+    return;
+  }
+  
+  IN_.Position(event);
+  lastEvent = event;
+}
+
+extern "C" {
+
+void uCHAN_MotorLeftActions (unsigned char name_)
+{
+	/* Accessing MESSAGE */
+	switch (name_)
+	{
+	case C2_BWD:
+		motorLeft.run(BACKWARD); 
+		break;
+	case C2_FWD:
+		motorLeft.run(FORWARD); 
+		break;
+	case C2_STOP:
+		motorLeft.run(RELEASE); 
+		break;
+	default: 
+		break;
+	}
+}
+
+void uCHAN_MotorRightActions (unsigned char name_)
+{
+	/* Accessing MESSAGE */
+	switch (name_)
+	{
+	case C3_BWD:
+		motorRight.run(BACKWARD); 
+		break;
+	case C3_FWD:
+		motorRight.run(FORWARD); 
+		break;
+	case C3_STOP:
+		motorRight.run(RELEASE); 
+		break;
+	default: 
+		break;
+	}
+}
+
+void iCHAN_(void)
+{
+		/* Initializing Input Error Function only if Input Error Option set */
+
+	#ifdef _EINPUT__
+		IN_.EINPUT_ = uEINPUT_;
+	#endif 
+
+		/* Initializing Output Interface */
+
+	OUT_.MotorLeftActions = uCHAN_MotorLeftActions;
+	OUT_.MotorRightActions = uCHAN_MotorRightActions;
+}
+}
+
+boolean IRcompare(int numpulses, int Signal[]) {
+  
+  for (int i=0; i< numpulses-1; i++) {
+    int oncode = pulses[i][1] * RESOLUTION / 10;
+    int offcode = pulses[i+1][0] * RESOLUTION / 10;
+    
+    /*
+    Serial.print(oncode); // the ON signal we heard
+    Serial.print(" - ");
+    Serial.print(Signal[i*2 + 0]); // the ON signal we want 
+    */
+    
+    // check to make sure the error is less than FUZZINESS percent
+    if ( abs(oncode - Signal[i*2 + 0]) <= (Signal[i*2 + 0] * FUZZINESS / 100)) {
+      //Serial.print(" (ok)");
+    } else {
+      //Serial.print(" (x)");
+      // we didn't match perfectly, return a false match
+      return false;
+    }
+    
+    /*
+    Serial.print("  \t"); // tab
+    Serial.print(offcode); // the OFF signal we heard
+    Serial.print(" - ");
+    Serial.print(Signal[i*2 + 1]); // the OFF signal we want 
+    */
+    
+    if ( abs(offcode - Signal[i*2 + 1]) <= (Signal[i*2 + 1] * FUZZINESS / 100)) {
+      //Serial.print(" (ok)");
+    } else {
+      //Serial.print(" (x)");
+      // we didn't match perfectly, return a false match
+      return false;
+    }
+    
+    //Serial.println();
+  }
+  // Everything matched!
+  return true;
+}
+
+int listenForIR(void) {
+  currentpulse = 0;
+  
+  while (1) {
+    uint16_t highpulse, lowpulse;  // temporary storage timing
+    highpulse = lowpulse = 0; // start out with no pulse length
+  
+//  while (digitalRead(IRpin)) { // this is too slow!
+    while (IRpin_PIN & (1 << IRpin)) {
+       // pin is still HIGH
+
+       // count off another few microseconds
+       highpulse++;
+       delayMicroseconds(RESOLUTION);
+
+       // If the pulse is too long, we 'timed out' - either nothing
+       // was received or the code is finished, so print what
+       // we've grabbed so far, and then reset
+       if ((highpulse >= MAXPULSE) && (currentpulse != 0)) {
+         return currentpulse;
+       }
+    }
+    // we didn't time out so lets stash the reading
+    pulses[currentpulse][0] = highpulse;
+  
+    // same as above
+    while (! (IRpin_PIN & _BV(IRpin))) {
+       // pin is still LOW
+       lowpulse++;
+       delayMicroseconds(RESOLUTION);
+       if ((lowpulse >= MAXPULSE)  && (currentpulse != 0)) {
+         return currentpulse;
+       }
+    }
+    pulses[currentpulse][1] = lowpulse;
+
+    // we read one high-low pulse successfully, continue!
+    currentpulse++;
+  }
+}
+
+void printpulses(void) {
+  Serial.println("\n\r\n\rReceived: \n\rOFF \tON");
+  for (uint8_t i = 0; i < currentpulse; i++) {
+    Serial.print(pulses[i][0] * RESOLUTION, DEC);
+    Serial.print(" usec, ");
+    Serial.print(pulses[i][1] * RESOLUTION, DEC);
+    Serial.println(" usec");
+  }
+  
+  // print it in a 'array' format
+  Serial.println("int IRsignal[] = {");
+  Serial.println("// ON, OFF (in 10's of microseconds)");
+  for (uint8_t i = 0; i < currentpulse-1; i++) {
+    Serial.print("\t"); // tab
+    Serial.print(pulses[i][1] * RESOLUTION / 10, DEC);
+    Serial.print(", ");
+    Serial.print(pulses[i+1][0] * RESOLUTION / 10, DEC);
+    Serial.println(",");
+  }
+  Serial.print("\t"); // tab
+  Serial.print(pulses[currentpulse-1][1] * RESOLUTION / 10, DEC);
+  Serial.print(", 0};");
+}
+
