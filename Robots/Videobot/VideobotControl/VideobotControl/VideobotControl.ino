8ee70c0868b7c3545d1d4decae5671051c0c410e
mnemonia <birkeland@gmx.net>
2012-05-22 14:37:05 +0200

    IR NEC protocol added

--- Robots/Videobot/VideobotControl/VideobotControl/VideobotControl.ino
+++ Robots/Videobot/VideobotControl/VideobotControl/VideobotControl.ino
@@ -1,51 +1,33 @@
+#define PLAY 0xEF50AF
+#define STOP 0xEF40BF
+#define FWD 0xEF0AF5
+#define BWD 0xEF2AD5
+
+#define IR_SIGNAL_COUNT 1
+#define JUMP_DELAY 1000
+
 #include <IRremote.h>
 
 IRsend irsend;
 
-const int PLAY = 0xEF40BF;
-const int STOP = 0xEF50AF;
-const int FWD = 0xEF0AF5;
-const int BWD = 0xEF2AD5;
+const int FWD_IDLE_PIN = 7;
+const int FWD_ACTIVE_PIN = 2;
+const int BWD_IDLE_PIN = 4;
+const int BWD_ACTIVE_PIN = 5;
+const int ON_PIN = 6;
+
+
 
 void setup() {         
 Serial.begin(9600);  
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(2, OUTPUT);     
-  pinMode(3, OUTPUT);     
-  pinMode(4, OUTPUT);     
-  pinMode(5, OUTPUT);     
-  pinMode(6, OUTPUT);     
-  pinMode(7, OUTPUT);     
+  pinMode(FWD_IDLE_PIN, OUTPUT);     
+  pinMode(FWD_ACTIVE_PIN, OUTPUT);     
+  pinMode(BWD_IDLE_PIN, OUTPUT);     
+  pinMode(BWD_ACTIVE_PIN, OUTPUT);     
+  pinMode(ON_PIN, OUTPUT);     
   pinMode(A0, INPUT);     
   pinMode(A1, INPUT);     
-  digitalWrite(6, HIGH);   // set the LED on
-  digitalWrite(7, HIGH);   // set the LED on
-}
-
-void play(){
-  Serial.println("Play");
-  sendCommand(PLAY);
-  digitalWrite(2, HIGH);   // set the LED on
-  digitalWrite(3, LOW);   // set the LED on
-  digitalWrite(4, HIGH);   // set the LED on
-  digitalWrite(5, LOW);   // set the LED on
-}
-void fwd(){
-  Serial.println("FWD");
-  sendCommand(FWD);
-  digitalWrite(2, LOW);   // set the LED on
-  digitalWrite(3, HIGH);   // set the LED on
-  digitalWrite(4, HIGH);   // set the LED on
-  digitalWrite(5, LOW);   // set the LED on
-}
-void bwd(){
-  Serial.println("BWD");
-  sendCommand(BWD);
-  digitalWrite(2, HIGH);   // set the LED on
-  digitalWrite(3, LOW);   // set the LED on
-  digitalWrite(4, LOW);   // set the LED on
-  digitalWrite(5, HIGH);   // set the LED on
+  digitalWrite(ON_PIN, HIGH);   // set the LED on
 }
 void loop() {
   int fwdControl = analogRead(A0);
@@ -54,21 +36,62 @@ void loop() {
   int bwdControl = analogRead(A1);
   Serial.print("BWD Control on A1: ");
   Serial.println(bwdControl);
-  if(fwdControl > 500 && bwdControl > 500){
+  int treshold = 900;
+  if(fwdControl > treshold && bwdControl > treshold){
     play();
   }
-  if(fwdControl < 500 && bwdControl > 500){
+  if(fwdControl < treshold && bwdControl > treshold){
     fwd();
+    delay(JUMP_DELAY);
   }
-  if(fwdControl > 500 && bwdControl < 500){
+  if(fwdControl > treshold && bwdControl < treshold){
     bwd();
+    delay(JUMP_DELAY);
   }
-  delay(50);              // wait for a second
+  delay(50);
 }
 
-void sendCommand(int command) {
-  for (int i = 0; i < 3; i++) {
-    irsend.sendNEC(command, 32); 
+void play(){
+  Serial.println("Play");
+  sendPlayCommand();
+  digitalWrite(FWD_IDLE_PIN, HIGH);   // set the LED on
+  digitalWrite(FWD_ACTIVE_PIN, LOW);   // set the LED on
+  digitalWrite(BWD_IDLE_PIN, HIGH);   // set the LED on
+  digitalWrite(BWD_ACTIVE_PIN, LOW);   // set the LED on
+}
+void fwd(){
+  Serial.println("FWD");
+  sendFwdCommand();
+  digitalWrite(FWD_IDLE_PIN, LOW);   // set the LED on
+  digitalWrite(FWD_ACTIVE_PIN, HIGH);   // set the LED on
+  digitalWrite(BWD_IDLE_PIN, HIGH);   // set the LED on
+  digitalWrite(BWD_ACTIVE_PIN, LOW);   // set the LED on  
+}
+void bwd(){
+  Serial.println("BWD");
+  sendBwdCommand();
+  digitalWrite(FWD_IDLE_PIN, HIGH);   // set the LED on
+  digitalWrite(FWD_ACTIVE_PIN, LOW);   // set the LED on
+  digitalWrite(BWD_IDLE_PIN, LOW);   // set the LED on
+  digitalWrite(BWD_ACTIVE_PIN, HIGH);   // set the LED on
+}
+
+void sendPlayCommand() {
+  for (int i = 0; i < IR_SIGNAL_COUNT; i++) {
+    irsend.sendNEC(0xEF50AF, 32); 
     delay(100);
   }
 }
+void sendFwdCommand() {
+  for (int i = 0; i < IR_SIGNAL_COUNT; i++) {
+    irsend.sendNEC(FWD, 32); 
+    delay(100);
+  }
+}
+void sendBwdCommand() {
+  for (int i = 0; i < IR_SIGNAL_COUNT; i++) {
+    irsend.sendNEC(BWD, 32); 
+    delay(100);
+  }
+}
+
