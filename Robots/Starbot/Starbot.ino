7645c974b84253b0644966e53740ed000ff7bd82
mnemonia <birkeland@gmx.net>
2012-06-04 00:34:26 +0200

    New robot Starbot

---
+++ Robots/Starbot/Starbot.ino
@@ -0,0 +1,24 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+
+void setup() {                
+  // initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards:
+  pinMode(12, OUTPUT);     
+  pinMode(13, OUTPUT);     
+  
+  digitalWrite(13, HIGH);   // set the LED on  
+}
+
+void loop() {
+  digitalWrite(12, HIGH);   // set the LED on
+//  digitalWrite(13, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(12, LOW);    // set the LED off
+//  digitalWrite(13, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+}
