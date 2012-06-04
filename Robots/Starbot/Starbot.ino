0398baba4a94540409506d235fb913ff6dc9ade2
mnemonia <birkeland@gmx.net>
2012-06-04 23:35:17 +0200

    Integrated simple servo behavior

--- Robots/Starbot/Starbot.ino
+++ Robots/Starbot/Starbot.ino
@@ -1,24 +1,36 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
+#include <Servo.h>
+
+Servo myservo;
+int pos = 0;    // variable to store the servo position 
+
 
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(12, OUTPUT);     
+  pinMode(11, OUTPUT);     
   pinMode(13, OUTPUT);     
   
-  digitalWrite(13, HIGH);   // set the LED on  
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+
+  digitalWrite(11, HIGH);   // set the LED on  
 }
 
 void loop() {
-  digitalWrite(12, HIGH);   // set the LED on
-//  digitalWrite(13, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(12, LOW);    // set the LED off
-//  digitalWrite(13, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
+  digitalWrite(13, HIGH);   // set the LED on
+  for(pos = 5; pos < 175; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  delay(1000);          
+  digitalWrite(13, LOW);
+  delay(1000);          
+  digitalWrite(13, HIGH);
+  for(pos = 175; pos>=5; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  digitalWrite(13, LOW);
+  delay(1000);
 }
