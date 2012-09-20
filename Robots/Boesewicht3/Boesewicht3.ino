9f35be288e90b0e234bcfb75e5ee786cc89b7f6a
mnemonia <birkeland@gmx.net>
2012-09-20 02:09:04 +0200

    Simplified Software Variant

---
+++ Robots/Boesewicht3/Boesewicht3.ino
@@ -0,0 +1,115 @@
+// Controlling a servo position using a potentiometer (variable resistor) 
+// by Michal Rinott <http://people.interaction-ivrea.it/m.rinott> 
+
+#include <Ultrasonic.h>
+#include <Servo.h> 
+ 
+Ultrasonic ultrasonic(A5,A4);
+const int ultrasonicClearThreshold = 68;
+const int ultrasonicSomethingThereThreshold = 64;
+const int ultrasonicAttentionThreshold = 55;
+const int ultrasonicDangerThreshold = 40;
+
+Servo leftServo;  // create servo object to control a servo 
+Servo rightServo;  // create servo object to control a servo 
+ 
+int potpin = 0;  // analog pin used to connect the potentiometer
+int distance;    // variable to read the value from the analog pin 
+ 
+void setup() 
+{ 
+  Serial.begin(9600);
+  pinMode(6,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+  leftServo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  rightServo.attach(10);  // attaches the servo on pin 9 to the servo object 
+  randomSeed(analogRead(A0));
+} 
+ 
+void loop() 
+{ 
+  distance = ultrasonic.Ranging(CM);
+//  Serial.println(val);
+  if(!isClear(distance)){
+    leftServo.write(withLeftMove(distance));                  // sets the servo position according to the scaled value 
+    delay(10);                           // waits for the servo to get there 
+    rightServo.write(withRightMove(distance));                  // sets the servo position according to the scaled value 
+    delay(10);                           // waits for the servo to get there 
+  }else{
+  
+  }
+
+  if(isAttention(distance)){
+    digitalWrite(6,LOW);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+  }else if(isDanger(distance)){
+    digitalWrite(6,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+  }else if(isSomethingThere(distance)){
+    digitalWrite(6,HIGH);
+    digitalWrite(12,LOW);
+    digitalWrite(13,HIGH);
+  }else{
+    digitalWrite(6,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(13,LOW);
+  }
+  
+  if(isDanger(distance)){
+    int noteDuration = distance>0 ? 1000/distance : 100;
+    tone(8, random(1, 20) * distance + 10, noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+  
+/*  Serial.print("#S|LOGTEST|[");
+  Serial.print(distance);
+  Serial.println("]#");*/
+  Serial.println(distance);
+  
+ // delay(30);                           // waits for the servo to get there 
+}
+
+int angle(int value){
+  return map(value, 30, 88, 60, 130);     // scale it to use it with the servo (value between 0 and 180) 
+}
+int withLeftMove(int value){
+/*  if(isDanger(value)){
+    return getRight(angle(value));
+  }
+  */
+  return getLeft(angle(value));
+}
+
+int withRightMove(int value){
+  if(isDanger(value)){
+    return getRight(angle(value));
+  }
+  return getLeft(angle(value));
+}
+boolean isClear(int value){
+  return value == 0 || value >= ultrasonicClearThreshold;
+}
+boolean isDanger(int value){
+  return value > 0 && value <= ultrasonicDangerThreshold;
+}
+boolean isAttention(int value){
+  return value > ultrasonicDangerThreshold && value <= ultrasonicAttentionThreshold;
+}
+boolean isSomethingThere(int value){
+  return value > ultrasonicAttentionThreshold && value <= ultrasonicSomethingThereThreshold;
+}
+int getLeft(int value){
+  return value;
+}
+int getRight(int value){
+  return 180 - getLeft(value);
+}
