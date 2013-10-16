ed3943305eafefc96ce6314d8a4fc214cd648ec0
mnemonia <birkeland@gmx.net>
2013-10-16 23:08:37 +0200

    Added light fade-in strategies for the steps

--- Home/Stairbot/Stairbot.ino
+++ Home/Stairbot/Stairbot.ino
@@ -1,23 +1,51 @@
 int steps[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 int stepCount = 12;
+boolean allTogether = false;
 
 void setup()  {
+  initializeLights();
+  if(allTogether){
+    fadeAllTogether();
+  }else{
+    fadeTopDownAndBottomUpAndMeetInTheMiddle();
+  }
+}
+
+void initializeLights(){
   for(int i=0; i<stepCount; i++){
     out(steps[i]);
   } 
 }
 
-void fade()  { 
+void fadeAllTogether()  { 
   for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) { 
-    fadeTo(fadeValue);
+    fadeAllToNext(fadeValue);
     delay(30);                            
   } 
 }
 
-void fadeTo(int fadeValue){
+void fadeAllToNext(int fadeValue){
   for(int i=0; i<stepCount; i++){
     analogWrite(steps[i], fadeValue);
   } 
 }
 
+void out(int pin){
+  pinMode(pin, OUTPUT);
+}
+
 void loop(){;}
+
+void fadeTopDownAndBottomUpAndMeetInTheMiddle(){
+  for(int i=0, j=stepCount-1; i<=j; i++,j--){
+    fadeTwoSteps(i,j);
+  }
+}
+
+void fadeTwoSteps(int stepOne, int stepTwo){
+  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) { 
+    analogWrite(steps[stepOne], fadeValue);
+    analogWrite(steps[stepTwo], fadeValue);
+    delay(30);                            
+  } 
+}
