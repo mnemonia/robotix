fbe99217054ba54e1bc88dbec583ae5f5b57fb0c
mnemonia <birkeland@gmx.net>
2013-10-16 22:46:16 +0200

    Light fade-in for 12 step stair

--- Home/Stairbot/Stairbot.ino
+++ Home/Stairbot/Stairbot.ino
@@ -1,61 +1,23 @@
-/* Knock Sensor
-  
-   This sketch reads a piezo element to detect a knocking sound. 
-   It reads an analog pin and compares the result to a set threshold. 
-   If the result is greater than the threshold, it writes
-   "knock" to the serial port, and toggles the LED on pin 13.
-  
-   The circuit:
-    * + connection of the piezo attached to analog in 0
-    * - connection of the piezo attached to ground
-    * 1-megohm resistor attached from analog in 0 to ground
+int steps[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+int stepCount = 12;
 
-   http://www.arduino.cc/en/Tutorial/Knock
-   
-   created 25 Mar 2007
-   by David Cuartielles <http://www.0j0.org>
-   modified 30 Aug 2011
-   by Tom Igoe
-   
-   This example code is in the public domain.
-
- */
- 
-
-// these constants won't change:
-const int ledPin = 13;      // led connected to digital pin 13
-const int knockSensor = A0; // the piezo is connected to analog pin 0
-const int threshold = 1000;  // threshold value to decide when the detected sound is a knock or not
-
-
-// these variables will change:
-int sensorReading = 0;      // variable to store the value read from the sensor pin
-int ledState = LOW;         // variable used to store the last LED status, to toggle the light
-
-void setup() {
- pinMode(ledPin, OUTPUT); // declare the ledPin as as OUTPUT
- Serial.begin(9600);       // use the serial port
+void setup()  {
+  for(int i=0; i<stepCount; i++){
+    out(steps[i]);
+  } 
 }
 
-void loop() {
-  // read the sensor and store it in the variable sensorReading:
-  sensorReading = analogRead(knockSensor);    
-//  Serial.println(sensorReading);
- // Serial.println(sensorReading);
-  // if the sensor reading is greater than the threshold:
-  if (sensorReading >= threshold) {
-    if(ledState == LOW){
-      // toggle the status of the ledPin:
-      ledState = true;   
-      // update the LED pin itself:        
-      // send the string "Knock!" back to the computer, followed by newline
-      Serial.println("Knock!");         
-    }else{
-      Serial.println("Press!");         
-    }
-  }else{
-    ledState = LOW;   
-  }
-  digitalWrite(ledPin, ledState);
-  delay(20);  // delay to avoid overloading the serial port buffer
+void fade()  { 
+  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) { 
+    fadeTo(fadeValue);
+    delay(30);                            
+  } 
 }
+
+void fadeTo(int fadeValue){
+  for(int i=0; i<stepCount; i++){
+    analogWrite(steps[i], fadeValue);
+  } 
+}
+
+void loop(){;}
