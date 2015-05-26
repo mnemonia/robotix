9b4da67b1fe52645e9f6dbbf66970db0ab8b765e
mnemonia <birkeland@gmx.net>
2015-05-26 21:11:52 +0200

    Cleaned up the code

--- Findling/OvenDoorController2/OvenDoorController2.ino
+++ Findling/OvenDoorController2/OvenDoorController2.ino
@@ -1,21 +1,12 @@
 #include "Arduino.h"
-#include <Adafruit_NeoPixel.h>
 #include "FindlingDoorController.h"
-//#include "FindlingMagnetDoorLock.h"
 
-//FindlingNeoPixel light_01 = FindlingNeoPixel(7, 0, 255, 255, 0, 26, 0);
-//FindlingMagnetDoorLock door_01 = FindlingMagnetDoorLock(5);
 FindlingDoorController door_01 = FindlingDoorController(5, 7, 0, 255, 255, 0, 26, 0);
-//Adafruit_NeoPixel a = Adafruit_NeoPixel(1, 7, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("findl.ing light controller");
-  //light_01.turnOn();
   door_01.shut();
-  //a.begin();
-  //a.setPixelColor(0, a.Color(0xFF, 0xCC, 0xFF));
-  //a.show();
 }
 
 void loop() {
